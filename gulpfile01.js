4773edfbd9766b4cebedf516819bfc42dbb24e89
Rayanne Moro <rayannecmoro@gmail.com>
2022-07-18 11:13:22 -0300

    curso

--- gulpfile01.js
+++ gulpfile01.js
@@ -1,71 +1,57 @@
-const { series, parallel} = require('gulp')
+const { series, parallel } = require('gulp')
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
 const image = require('gulp-image')
-const stripJs = require('gulp-strip-comments')
+// const stripJs = require('gulp-strip-comments')
 const stripCss = require('gulp-strip-css-comments')
 const htmlmin = require('gulp-htmlmin')
 const babel = require('gulp-babel')
 const browserSync = require('browser-sync').create()
-const sass = require('gulp-sass')( require('node-sass'))
-const { pipe } = require('stdout-stream')
-const { contains } = require('jquery')
-const reload = browserSync.reload
+const reload = browserSync.reload()
 
 function tarefasCSS(cb) {
 
     gulp.src([
-            './node_modules/bootstrap/dist/css/bootstrap.css',
+            './src/css/**/*.css'
+            // './node_modules/bootstrap/dist/css/bootstrap.css',
             // './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
             // './vendor/owl/css/owl.css',
-            // './vendor/jquery-ui/jquery-ui.css'
+            // './vendor/jquery-ui/jquery-ui.css',
+            // './src/css/style.css'
         ])
         .pipe(stripCss())                   // remove comentários css   
-        .pipe(concat('libs.css'))           // mescla arquivos
+        .pipe(concat('styles.css'))         // mescla arquivos
         .pipe(cssmin())                     // minifica css
-        .pipe(rename({ suffix: '.min'}))    // libs.min.css
+        .pipe(rename({ suffix: '.min'}))    // styles.min.css
         .pipe(gulp.dest('./dist/css'))      // cria arquivo em novo diretório
 
     cb()
 
 }
 
-function tarefasSASS(cb) {
-    
-    gulp.src('./src/scss/**/*.scss')
-        .pipe(sass()) // transforma o sass para css
-        .pipe(gulp.dest('./dist/css')) 
-
-    cb()
-}
-
-function tarefasJS(callback){
-
+function tarefasJS(callback) {
     gulp.src([
-            './node_modules/jquery/dist/jquery.js',
-            './node_modules/bootstrap/dist/js/bootstrap.js',
-            // './vendor/owl/js/owl.js',
-            // './vendor/jquery-mask/jquery.mask.js',
-            // './vendor/jquery-ui/jquery-ui.js',
-            './src/js/form.js'
-        ])
+        // './node_modules/jquery/dist/jquery.js',
+        // './node_modules/bootstrap/dist/js/bootstrap.js',
+        './src/js/**/*.js'
+    ])
         .pipe(babel({
-            comments: false,
+            comments: false, 
             presets: ['@babel/env']
         }))
-        .pipe(concat('scripts.js'))         // mescla arquivos
-        .pipe(uglify())                     // minifica js
-        .pipe(rename({ suffix: '.min'}))    // scripts.min.js
-        .pipe(gulp.dest('./dist/js'))       // cria arquivo em novo diretório
-
+        .pipe(concat('libs.js'))
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./dist/js'))
+    
     return callback()
 }
 
-function tarefasImagem(){
-    
+function tarefasImages() {
+   
     return gulp.src('./src/images/*')
         .pipe(image({
             pngquant: true,
@@ -82,15 +68,14 @@ function tarefasImagem(){
 }
 
 // POC - Proof of Concept
-function tarefasHTML(callback){
-
+function tarefasHTML(callback) {
     gulp.src('./src/**/*.html')
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest('./dist'))
 
     return callback()
-
 }
+
 
 gulp.task('serve', function(){
 
@@ -104,17 +89,22 @@ gulp.task('serve', function(){
 
 })
 
-function end(cb){
-    console.log("tarefas concluídas")
-    return cb()
-}
-
 // series x parallel
-const process = parallel( tarefasHTML, tarefasJS, tarefasCSS, tarefasSASS, end)
+const process = series( tarefasHTML, tarefasCSS, tarefasJS )
 
-exports.styles = tarefasCSS
+exports.css = tarefasCSS
 exports.scripts = tarefasJS
-exports.images = tarefasImagem
-exports.sass = tarefasSASS
+exports.image = tarefasImages
+// exports.html = tarefasHTML
 
 exports.default = process
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    })
+    gulp.watch('./src/**/*').on('change', process)
+    gulp.watch('./src/**/*').on('change', reload)
+})
